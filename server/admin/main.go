1a711bfbfaf34e44d3cd11bd5ff7eb1dac855364
Prateek Sharma <prateeksharma.2801@gmail.com>
2025-04-16 15:20:20 +0530

    Adding authorization to list of allowed headers

--- server/admin/main.go
+++ server/admin/main.go
@@ -23,7 +23,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowHeaders:     "Authorization,Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
 		AllowOrigins:     "http://localhost:3000",
 		AllowCredentials: true,
 		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
